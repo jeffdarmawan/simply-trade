50ebaaf341e65d2140a988ecfecae332d22514eb
bit-fin-jay <t.jaydenlzx@gmail.com>
2024-04-14 20:15:17 +0800

    test
    test

--- live_trading/trade.py
+++ live_trading/trade.py
@@ -1,6 +1,7 @@
-from signal_generator import get_current_price, get_top10_features, fetch_candlestick_data
-from risk_manager import get_current_price, get_instrument_precision, get_current_balance, get_quantity,get_pnl_price,get_open_positions, check_instrument_positions,place_market_order, close_position, calculate_total_unrealised_pnl, close_all_trades
-
+from returns import calculate_annualised_returns, fetch_transactions, process_transactions, calculate_drawdowns, fetch_balance_changes,calculate_sharpe_ratio, fetch_closed_trades, calculate_win_rate_from_trades, pnl_all_positions
+from signal_generator import get_current_price, data_preparation
+from risk_manager import get_current_price, get_current_balance, get_quantity,get_pnl_price,get_open_positions, check_instrument_positions,place_market_order, close_position, calculate_total_unrealised_pnl, close_all_trades
+from datetime import datetime
 import time
 import oandapyV20
 import oandapyV20.endpoints.pricing as pricing
@@ -42,12 +43,15 @@ model_15mins_features = ['DPO_20', 'THERMO_20_2_0.5', 'CFO_9', 'TRUERANGE_1', 'P
 default_take_profit = 0.015
 default_stop_loss = 0.005
 
+target_pnl = 105000 
+stoploss_pnl = 95000
+
 # Setup
 default_usd_pairs = ["EUR_USD","USD_JPY", "GBP_USD","AUD_USD"]
 
 # trade_cycle
 default_trade_cycle = 1
-
+target_pnl = 10500
 # order type
 default_order_type = "MARKET"
 
@@ -70,41 +74,46 @@ def trade_attempt(
     if status == Status.Active:       
         try:
             print("Running strategy...")
+            opening_balance = get_current_balance()
+            start_time = datetime.now()
+            open_datetime = datetime.now().date()
 
             positions_dict = get_open_positions()
+            print(instrument)
 
             for instrument in usd_pairs:
-                print(instrument)
+
+                 # === Model Selection
                 price = get_current_price(instrument)
-                price_df = fetch_candlestick_data(instrument, lookback_count)
-                
-                # === some model logic
-                
-                # if model == Models.FiveMin:
-                    # get_top10_features_5mins()
-                # elif model == Model.FifteenMin:
-                    # get_top10_features_15mins()
+                if model == Models.OneMin:
+                    model_type = "models_1mins"
+                    price_df = data_preparation(model_type, instrument,lookback_count)
 
-                top10_df3 = get_top10_features(price_df["m_open"], price_df["m_high"], price_df["m_low"], price_df["m_close"],model_15mins_features)
-                price_df = pd.concat([price_df, top10_df3], axis=1)
-                model_data = price_df[model_15mins_features]
+                elif model == Models.FiveMin:
+                    model_type = "models_5mins"
+                    price_df = data_preparation(model_type, instrument,lookback_count)
 
-                # make prediction for the next period price direction
-                # get the currency pair's mode
-                model = models_15[pairs_mapping[instrument]]
+                elif model == Models.FifteenMin:
+                    model_type = "models_15mins"
+                    price_df = data_preparation(model_type, instrument,lookback_count)
 
-                # save the predicted y direction
-                #   1 is positive => buy t+1, 0 is not positive => sell t+1
-                y_pred = model.predict(model_data)
+                elif model == Models.OneHour: 
+                    model_type = "models_1hour"
+                    price_df = data_preparation(model_type, instrument,lookback_count)
+
+                else:   
+                    model_type = "models_15mins"
+                    price_df = data_preparation(model_type, instrument,lookback_count)
 
                 # === end of model logic
 
-                price_df["prediction_on_next_close"] = [1 if x > 0 else -1 for x in y_pred]
                 #find the last signal
                 signal = price_df["prediction_on_next_close"].iloc[-1]
-
+                positions_dict = get_open_positions()
                 position_type = check_instrument_positions(positions_dict,instrument)
-                #
+                
+                #Determine position and create market order
+
                 if position_type == "None": 
                     if signal == 1: 
                         quantity = get_quantity(instrument,signal)
@@ -145,6 +154,47 @@ def trade_attempt(
                         print ("Hold the trade")
                 else:
                     pass  
+            
+            #Calculate the pnl
+            
+            positions_dict = get_open_positions()
+            long_pnl, short_pnl, total_pnl = calculate_total_unrealised_pnl(positions_dict)    
+
+            print(f" Target:  {target_pnl:.2f} | StopLoss: {stoploss_pnl :.2f} | PNL:  {total_pnl:.2f} ") 
+            #calculate returns 
+            cycle_datetime = datetime.now().date()
+            trade_days = (open_datetime - cycle_datetime).days
+            annualised_returns = calculate_annualised_returns(opening_balance, trade_days)
+            #print( "Annualised return is ", round(annualised_returns, 2) )
+
+            #calculate draw_down
+            transactions = fetch_transactions(start_time, datetime.now())
+            df_equity = process_transactions(transactions)
+            df_drawdown = calculate_drawdowns(df_equity)
+            max_drawdown = df_drawdown['drawdown_pct'].max()
+            #print(f"Maximum Drawdown: {max_drawdown}%")
+
+            #Calculate sharpe ratio
+            #Fetch account changes
+            df_returns = fetch_balance_changes(start_time, datetime.now())
+
+            if not df_returns.empty:
+                sharpe_ratio = calculate_sharpe_ratio(df_returns)
+                #print( "Sharpe Ratio is ", round(sharpe_ratio, 2) )
+            else:
+                print("No data available to calculate Sharpe Ratio.")
+
+            #Calculate Win-rate
+            closed_trades = fetch_closed_trades()
+            win_rate = calculate_win_rate_from_trades(closed_trades)
+            #print(f"Win Rate: {win_rate:.2f}%")
+
+            #Update Time series PNL
+            time_series = []
+            pnl_series = []
+            pnl = pnl_all_positions()
+            time_series.append(datetime.now())
+            pnl_series.append(pnl)
 
             time.sleep(trade_cycle)  # Wait for a minute before the next cycle
 
@@ -156,9 +206,11 @@ def trade_attempt(
         
         try: 
             print("Trade is still open")
+
             positions_dict = get_open_positions()
-            # print(positions_dict)
-            # print("Current balance: {:.2f}".format(get_current_balance()))
+            long_pnl, short_pnl, total_pnl = calculate_total_unrealised_pnl(positions_dict)    
+
+            print(f" Target:  {target_pnl:.2f} | StopLoss: {stoploss_pnl :.2f} | PNL:  {total_pnl:.2f} ")
 
         except Exception as e:
             print(f"An error occurred: {e}")
@@ -168,3 +220,4 @@ def trade_attempt(
         print("Closing all Trades")
         close_all_trades(client, accountID)
         print("Current balance: {:.2f}".format(get_current_balance()))
+    
